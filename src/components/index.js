c81c8161ec4c42cacfd47e13b34b98824b82090a
Darlan <darlan.silvadacruz@gmail.com>
2021-12-29 21:02:17 -0300

    create index.js in components folder to export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header/Header';
+export { default as Scoreboard } from './Scoreboard/Scoreboard';
+export { default as ButtonsGrid } from './ButtonsGrid/ButtonsGrid'
+export { default as Button } from './Button/Button';
+export { default as RulesButton } from './RulesButton/RulesButton';
