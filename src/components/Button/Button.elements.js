934228294dc6bda4a2872a0ee478e479ec9f9700
Darlan <darlan.silvadacruz@gmail.com>
2022-01-01 21:30:58 -0300

    add winner style in button component

--- src/components/Button/Button.elements.js
+++ src/components/Button/Button.elements.js
@@ -4,7 +4,7 @@ export const ButtonContainer = styled.button`
   align-items: center;
   background: ${({ theme }) => `linear-gradient(180deg, ${theme.colors[0]}, ${theme.colors[1]})`};
   border-radius: 100%;
-  box-shadow: ${({ theme }) => `0 5px ${theme.colors[2]}`};
+  box-shadow: ${({ theme, winner }) => winner ? `0 5px ${theme.colors[2]}, 0 0 0 4vw hsla(0, 0%, 100%, 4%), 0 0 0 9vw hsla(0, 0%, 100%, 3%),  0 0 0 17vw hsla(0, 0%, 100%, 1.5%)` : ` 0 5px ${theme.colors[2]}`};
   display: flex;
   height: 32vw;
   justify-content: center;
