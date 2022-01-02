cd7cdf0a1fe43fd3d96d018d94c3a914230907e0
Darlan <darlan.silvadacruz@gmail.com>
2022-01-01 21:05:40 -0300

    remove react fragment in button grid component

--- src/components/ButtonsGrid/ButtonsGrid.js
+++ src/components/ButtonsGrid/ButtonsGrid.js
@@ -3,10 +3,8 @@ import { ButtonsGridContainer } from "./ButtonsGrid.elements";
 
 export default function ButtonsGrid({ children }) {
   return (
-    <React.Fragment>
-      <ButtonsGridContainer>
-        {children}
-      </ButtonsGridContainer>
-    </React.Fragment>
+    <ButtonsGridContainer>
+      {children}
+    </ButtonsGridContainer>
   );
 }
