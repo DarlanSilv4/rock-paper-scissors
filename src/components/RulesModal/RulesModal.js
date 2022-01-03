8e1067ce47e60058c94f5a745fc97d3b817569e5
Darlan <darlan.silvadacruz@gmail.com>
2022-01-02 23:09:09 -0300

    setup exit button in the rules modal component

--- src/components/RulesModal/RulesModal.js
+++ src/components/RulesModal/RulesModal.js
@@ -1,12 +1,16 @@
+import { useGame } from "../../contexts/GameContext";
 import { RulesImg, RulesModalBackground, RulesModalContainer, RulesModalExitButton, RulesModalTitle } from "./RulesModal.elements";
 
 export default function RulesModal() {
+
+  const { showRules } = useGame();
+
   return (
     <RulesModalBackground>
       <RulesModalContainer>
         <RulesModalTitle>Rules</RulesModalTitle>
         <RulesImg src="/images/image-rules.svg" />
-        <RulesModalExitButton>
+        <RulesModalExitButton onClick={() => showRules()}>
           <img src="/images/icon-close.svg" />
         </RulesModalExitButton>
       </RulesModalContainer>
