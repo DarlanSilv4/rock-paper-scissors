59740f6d719000393976127a71d0c91486ea2941
Darlan <darlan.silvadacruz@gmail.com>
2022-01-02 23:07:52 -0300

    setup rules button component

--- src/components/RulesButton/RulesButton.js
+++ src/components/RulesButton/RulesButton.js
@@ -1,8 +1,12 @@
+import { useGame } from "../../contexts/GameContext"
 import { RulesButtonContainer } from "./RulesButton.elements"
 
 export default function RulesButton() {
+
+  const { showRules } = useGame();
+
   return (
-    <RulesButtonContainer>
+    <RulesButtonContainer onClick={() => showRules()}>
       <span>Rules</span>
     </RulesButtonContainer>
   )
