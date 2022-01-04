90f60b7608d81e62efd9ac1c1d0b65f61bd6a64d
Darlan <darlan.silvadacruz@gmail.com>
2022-01-03 21:39:47 -0300

    set up components on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import GlobalStyle, { Container } from "./globalStyles";
+import { Header, RulesButton, RulesModal, ResultGrid, ButtonsGrid } from "./components";
+import { useGame } from "./contexts/GameContext";
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  const { result, isRulesModalDisplayed } = useGame();
+
+  return (
+    <React.Fragment>
+      {isRulesModalDisplayed ? <RulesModal /> : <React.Fragment></React.Fragment>}
+      <GlobalStyle />
+      <Container>
+        <Header />
+        {result != null ? <ResultGrid /> : <ButtonsGrid />}
+        <RulesButton />
+      </Container>
+    </React.Fragment>
   );
 }
 
