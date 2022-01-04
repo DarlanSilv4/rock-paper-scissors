a20b0d0740c34e6c1fe6083738bee04adf307024
Darlan <darlan.silvadacruz@gmail.com>
2022-01-03 23:57:08 -0300

    add css animation in button component

--- src/components/Button/Button.elements.js
+++ src/components/Button/Button.elements.js
@@ -1,10 +1,25 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const rippleShadow = (theme) => keyframes`
+  0% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 1vw hsla(0, 0%, 100%, 4%), 0 0 0 2vw hsla(0, 0%, 100%, 3%), 0 0 0 3vw hsla(0, 0%, 100%, 1.5%);} 
+  10% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 2vw hsla(0, 0%, 100%, 4%), 0 0 0 3vw hsla(0, 0%, 100%, 3%), 0 0 0 4vw hsla(0, 0%, 100%, 1.5%);} 
+  20% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 3vw hsla(0, 0%, 100%, 4%), 0 0 0 5vw hsla(0, 0%, 100%, 3%), 0 0 0 8vw hsla(0, 0%, 100%, 1.5%);} 
+  30% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 4vw hsla(0, 0%, 100%, 4%), 0 0 0 7vw hsla(0, 0%, 100%, 3%), 0 0 0 12vw hsla(0, 0%, 100%, 1.5%);} 
+  40% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 4vw hsla(0, 0%, 100%, 4%), 0 0 0 9vw hsla(0, 0%, 100%, 3%), 0 0 0 17vw hsla(0, 0%, 100%, 1.5%);} 
+  50% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 4vw hsla(0, 0%, 100%, 4%), 0 0 0 9vw hsla(0, 0%, 100%, 3%), 0 0 0 17vw hsla(0, 0%, 100%, 1.5%);} 
+  60% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 3vw hsla(0, 0%, 100%, 4%), 0 0 0 7vw hsla(0, 0%, 100%, 3%), 0 0 0 12vw hsla(0, 0%, 100%, 1.5%);} 
+  70% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 2vw hsla(0, 0%, 100%, 4%), 0 0 0 5vw hsla(0, 0%, 100%, 3%), 0 0 0 8vw hsla(0, 0%, 100%, 1.5%);} 
+  80% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 1vw hsla(0, 0%, 100%, 4%), 0 0 0 3vw hsla(0, 0%, 100%, 3%), 0 0 0 4vw hsla(0, 0%, 100%, 1.5%);} 
+  90% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 1vw hsla(0, 0%, 100%, 4%), 0 0 0 2vw hsla(0, 0%, 100%, 3%), 0 0 0 3vw hsla(0, 0%, 100%, 1.5%);} 
+  100% {box-shadow: 0 5px ${theme.colors[2]}, 0 0 0 1vw hsla(0, 0%, 100%, 4%), 0 0 0 2vw hsla(0, 0%, 100%, 3%), 0 0 0 3vw hsla(0, 0%, 100%, 1.5%);} 
+`;
 
 export const ButtonContainer = styled.button`
+  animation: ${({ theme, winner }) => winner && rippleShadow(theme)} 2s linear infinite;
   align-items: center;
   background: ${({ theme }) => `linear-gradient(180deg, ${theme.colors[0]}, ${theme.colors[1]})`};
   border-radius: 100%;
-  box-shadow: ${({ theme, winner }) => winner ? `0 5px ${theme.colors[2]}, 0 0 0 4vw hsla(0, 0%, 100%, 4%), 0 0 0 9vw hsla(0, 0%, 100%, 3%),  0 0 0 17vw hsla(0, 0%, 100%, 1.5%)` : ` 0 5px ${theme.colors[2]}`};
+  box-shadow: ${({ theme }) => ` 0 5px ${theme.colors[2]}`};
   display: flex;
   height: 32vw;
   justify-content: center;
